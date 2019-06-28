2290ac23a83e744422ad61169abf59422feccf0b
Marcos García <thiamath@gmail.com>
2019-06-28 19:41:38 +0000

    Initial commit

---
+++ src/main/kotlin/GradleStarter.kt
@@ -0,0 +1,29 @@
+package org.example
+
+import java.time.LocalDateTime.now
+
+import com.hexagonkt.http.httpDate
+import com.hexagonkt.http.server.*
+import com.hexagonkt.http.server.jetty.JettyServletAdapter
+import com.hexagonkt.injection.InjectionManager.bindObject
+
+val server: Server by lazy {
+    Server {
+        before {
+            response.setHeader("Server", "Servlet/3.1")
+            response.setHeader("Transfer-Encoding", "chunked")
+            response.setHeader("Date", httpDate(now()))
+        }
+
+        get("/text") { ok("Hello, World!", "text/plain") }
+    }
+}
+
+/**
+ * Start the service from the command line.
+ */
+fun main() {
+    bindObject<ServerPort>(JettyServletAdapter())
+    server.start()
+}
+
